1e7852e36b557396d0a527fd3f999a76e2f6d415
santiagnohra <santiagnohra1@gmail.com>
2022-11-01 10:57:48 -0500

    Avances en Cython, reestructuracion y multihilos implementado

--- Planeta_cy.pyx
+++ Planeta_cy.pyx
@@ -1,15 +1,23 @@
-from math import sqrt
+#cython: language_level=3
+cimport cython
+
+'''
+Fecha: 1 nov 2022
+Autor: Santiago Nohra
+Tema: Cython
+'''
+
 '''
 Se requiere la raiz cuadrada:
     Se instancia como funcion externa
-    y se prepara para multihilo
+    y se prepara para multihilo. nogil = Bloqueo global
 '''
-#cdef extern from "math.h":
-#    double sqrt(double x) nogil
+cdef extern from "math.h":
+    double sqrt(double x) nogil
 
 cdef class Planet(object):
     #Declaracion de variables publicas
-    cdef public double x, y, z, vx, vy, vz, m
+    cdef public float x, y, z, vx, vy, vz, m
 
     def __init__(self):
         #Posicion y velocidad (3D)
@@ -23,15 +31,26 @@ cdef class Planet(object):
         #Masa
         self.m = 1.0
 
-cdef void single_step(Planet planet, double dt):
+
+
+'''
+La distancia no puede ser 0
+Para esto se hace una alerta basada en cython: cdivision(True/False)
+Al poner true, invalida la instruccion al saltar la bandera (INF)
+Se prepara con un decorador de cython
+'''
+@cython.cdivision(True)
+cdef void single_step(Planet planet, double dt) nogil:
     '''Dar un paso'''
 
+    #Se declaran las variables usadas
+    cdef double distance, Fx, Fy, Fz 
 
     #Calcular fuerza: gravedad al origen
-    cdef double distance = sqrt(planet.x**2 + planet.y**2 + planet.z**2)
-    cdef double Fx = -planet.x / distance**3
-    cdef double Fy = -planet.y / distance**3
-    cdef double Fz = -planet.z / distance**3
+    distance = sqrt(planet.x**2 + planet.y**2 + planet.z**2)
+    Fx = -planet.x / distance**3
+    Fy = -planet.y / distance**3
+    Fz = -planet.z / distance**3
 
     #Posicion a partir de la velocidad y el tiempo
     planet.x += dt * planet.vx
@@ -43,10 +62,17 @@ cdef void single_step(Planet planet, double dt):
     planet.vy += dt * Fy / planet.m
     planet.vz += dt * Fz / planet.m
  
-def step_time(planet, time_span, n_steps):
+def step_time(Planet planet, double time_span, int n_steps):
     '''Dar n cantidad de pasos'''
-    cdef double dt = time_span / n_steps
+    cdef double dt
     cdef int j
-    for j in range(n_steps):
-        single_step(planet, dt)
+
+    dt = time_span / n_steps
+
+    '''
+    Se prepara para el paralelismo
+    '''
+    with nogil:
+        for j in range(n_steps):
+            single_step(planet, dt)
 
--- main.py
+++ main.py
@@ -1,24 +1,70 @@
+'''
+Fecha: 1 nov 2022
+Autor: Santiago Nohra
+Tema: Cython
+La idea es exportar un .csv con la toma de datos
+'''
+
 import Planeta_cy
 import Planeta_py
 import time
 
-steps=1000000
-time_frame = 100
 
-planet_cy = Planeta_cy.Planet()
+#Se inicializa planeta para python, datos de wikipedia (Tierra)
 planet_py = Planeta_py.Planet()
+planet_py.x = 100*10**3
+planet_py.y = 300*10**3
+planet_py.z = 700*10**3
 
-init_time=time.time()
-Planeta_py.step_time(planet_py, time_frame, steps)
-end_time=time.time()
+planet_py.vx = 2.00*10**3
+planet_py.vy = 29.87*10**3
+planet_py.vz = 0.034*10**3
 
-time_python = end_time - init_time
+planet_py.m = 5.97424*10**24
 
-init_time=time.time()
-Planeta_cy.step_time(planet_cy, time_frame, steps)
-end_time=time.time()
+#Se inicializa planeta para python, datos de wikipedia (Tierra)
+planet_cy = Planeta_cy.Planet()
+planet_cy.x = 100*10**3
+planet_cy.y = 300*10**3
+planet_cy.z = 700*10**3
 
-time_cython = end_time - init_time
+planet_cy.vx = 2.00*10**3
+planet_cy.vy = 29.87*10**3
+planet_cy.vz = 0.034*10**3
+
+planet_cy.m = 5.97424*10**24
+
+#Se consideran las otras variables
+steps = 2000000
+time_span = 400
+
+#Formato para el fichero de salida
+formato_datos = "{:.5f},{:.5f}\n"
+
+
+#Definicion de experimentos, reduccion de ruido gaussiano
+for i in range(5):
+
+    #Toma de tiempos para python
+    init_time=time.time()
+    Planeta_py.step_time(planet_py, time_span, steps)
+    end_time=time.time()
+
+    time_python = end_time - init_time
+
+    #Toma de tiempos para cython
+    init_time=time.time()
+    Planeta_cy.step_time(planet_cy, time_span, steps)
+    end_time=time.time()
+
+    time_cython = end_time - init_time
+
+    with open("planeta.csv", "a") as archivo:
+        archivo.write(formato_datos.format(time_python, time_cython))
+
+
+
+
 
 print("Tiempo de ejecucion con python: {}s\nTiempo de ejcucion con cython: {}s".format(time_python, time_cython))
 
